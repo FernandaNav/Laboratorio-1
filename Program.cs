09ecf07cb1bc5475a7a0105c6cb685bd06957c26
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2024-07-29 09:10:15 -0600

    Cambio2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
-﻿Console.WriteLine("___________");
+﻿int opcion;
+
+Console.WriteLine("___________");
 Console.WriteLine("BIENVENIDO");
 Console.WriteLine("___________");
-Console.WriteLine("");
+Console.WriteLine("Bienvenido a la papelería CHUCHITO...");
+Console.WriteLine("¿Qué deseas realizar?");
+Console.WriteLine("Opción 1: Agregar producto.");
+Console.WriteLine("Opción 2: Comprar productos.");
+
+try
+{
+    opcion = Convert.ToInt32
+}
+catch (FormatException)
+{
+
+}
+
+
 
