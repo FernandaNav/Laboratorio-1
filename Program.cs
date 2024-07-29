ad6f816b07693337218b8d42bb28d67b0e2b9dbe
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2024-07-29 09:17:05 -0600

    cambio3

--- Program.cs
+++ Program.cs
@@ -1,21 +1,43 @@
-﻿int opcion;
+﻿int opcion=0;
 
-Console.WriteLine("___________");
-Console.WriteLine("BIENVENIDO");
-Console.WriteLine("___________");
-Console.WriteLine("Bienvenido a la papelería CHUCHITO...");
-Console.WriteLine("¿Qué deseas realizar?");
-Console.WriteLine("Opción 1: Agregar producto.");
-Console.WriteLine("Opción 2: Comprar productos.");
-
-try
+do
 {
-    opcion = Convert.ToInt32
-}
-catch (FormatException)
-{
+    Console.WriteLine("___________");
+    Console.WriteLine("BIENVENIDO");
+    Console.WriteLine("___________");
+    Console.WriteLine("Bienvenido a la papelería CHUCHITO...");
+    Console.WriteLine("¿Qué deseas realizar?");
+    Console.WriteLine("Opción 1: Agregar producto.");
+    Console.WriteLine("Opción 2: Comprar productos.");
+    Console.WriteLine("Opción 3: Salir.");
 
+    try
+    {
+        opcion = Convert.ToInt32(Console.ReadLine());
+        switch (opcion)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                Console.Clear();
+                Console.WriteLine("Ten un buen día");
+                break;
+        }
+    }
+    catch (FormatException)
+    {
+        MensajeDeError();
+    }
+} while (opcion != 3);
+
+
+static void MensajeDeError()
+{
+    Console.WriteLine("Error de Formato");
 }
+
 
 
 
